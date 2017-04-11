2462f2a1183696b700125354069ebaf010605c79
gemdude46 <gemdude46@gmail.com>
2017-04-11 19:43:21 +0100

    cant into indentation

--- code/Page_20_22.py
+++ code/Page_20_22.py
@@ -4,14 +4,14 @@ pygame.init()
 screen = pygame.display.set_mode((640,480))
 xpos = 50
 while 1:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			sys.exit()
-	pressed_keys = pygame.key.get_pressed()
-	if pressed_keys[K_RIGHT]:
-		xpos += 1
-	if pressed_keys[K_LEFT]:
-		xpos -= 1
-	screen.fill((255,255,255))
-	pygame.draw.circle(screen,(0,255,0),(xpos,200),20)
-	pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+    pressed_keys = pygame.key.get_pressed()
+    if pressed_keys[K_RIGHT]:
+        xpos += 1
+    if pressed_keys[K_LEFT]:
+        xpos -= 1
+    screen.fill((255,255,255))
+    pygame.draw.circle(screen,(0,255,0),(xpos,200),20)
+    pygame.display.update()
